06c363802a32f5297e990421e9918a5024acdb7a
mmusich <marco.musich@cern.ch>
2017-07-27 17:38:27 +0200

    for comparison of PV files

--- CheckingTools/MyCompareHistsWithRatio.C
+++ CheckingTools/MyCompareHistsWithRatio.C
@@ -51,11 +51,25 @@ void compareAll(TString file1,TString file2){
 	  if (obj->IsA()->InheritsFrom("TH2")) continue;
 	  TCanvas *c1 = new TCanvas(h->GetName(),h->GetName(),800,600);
 	  c1->cd();
+	  TPad *pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1.0);
+	  pad1->SetBottomMargin(0.01); // Upper and lower plot are joined
+	  //	  pad1->SetGridx();         // Vertical grid
+	  pad1->Draw();             // Draw the upper pad: pad1
+	  pad1->cd();               // pad1 becomes the current pad
+	  
 	  h->SetMarkerColor(kRed);
+	  h->SetStats(kFALSE);
 	  h->SetLineColor(kRed);
 	  h->SetMarkerStyle(kOpenSquare);
+	  h->GetXaxis()->SetLabelOffset(0.2);
+
+	  h->GetYaxis()->SetTitleSize(0.05);
+	  //h->GetYaxis()->SetTitleFont(43);
+	  // h->GetYaxis()->SetTitleOffset(0.04);
+
 	  h->Draw();
 	  TH1 *h2 = (TH1*)f2->Get(fullpath.Data());
+	  h2->SetStats(kFALSE);
 
 	  if(h2==nullptr){
 	    std::cout<<"WARNING!!!!!! "<<fullpath<<" does NOT exist in second file!!!!!"<<std::endl;
@@ -65,10 +79,49 @@ void compareAll(TString file1,TString file2){
 	  h2->SetMarkerColor(kBlue);
 	  h2->SetLineColor(kBlue);
 	  h2->SetMarkerStyle(kOpenCircle);
+	  h2->GetXaxis()->SetLabelOffset(0.2);
 	  h2->Draw("same");
 	  TString savename = fullpath.ReplaceAll("/","_");
 	  double ksProb = 0;
 	  
+	   // lower plot will be in pad
+	  c1->cd();          // Go back to the main canvas before defining pad2
+	  TPad *pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
+	  pad2->SetTopMargin(0.01);
+	  pad2->SetBottomMargin(0.35);
+	  pad2->SetGridy(); // horizontal grid
+	  pad2->Draw();
+	  pad2->cd();       // pad2 becomes the current pad
+	  
+	  // Define the ratio plot
+	  TH1F *h3 = (TH1F*)h->Clone("h3");
+	  h3->SetLineColor(kBlack);
+	  h3->SetMarkerColor(kBlack);
+	  h3->SetTitle("");
+	  h3->SetMinimum(0.55);  // Define Y ..
+	  h3->SetMaximum(1.55);  // .. range
+	  h3->SetStats(0);       // No statistics on lower plot
+	  h3->Divide(h2);
+	  h3->SetMarkerStyle(20);
+	  h3->Draw("ep");       // Draw the ratio plot
+
+	  // Y axis ratio plot settings
+	  h3->GetYaxis()->SetTitle("ratio");
+	  h3->GetYaxis()->SetNdivisions(505);
+	  h3->GetYaxis()->SetTitleSize(20);
+	  h3->GetYaxis()->SetTitleFont(43);
+	  h3->GetYaxis()->SetTitleOffset(1.2);
+	  h3->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+	  h3->GetYaxis()->SetLabelSize(15);
+	  
+	  // X axis ratio plot settings
+	  h3->GetXaxis()->SetTitleSize(20);
+	  h3->GetXaxis()->SetTitleFont(43);
+	  h3->GetXaxis()->SetTitleOffset(4.);
+	  h3->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+	  h3->GetXaxis()->SetLabelSize(15);
+	  h3->GetXaxis()->SetLabelOffset(0.02);
+
 	  // avoid spurious false positive due to empty histogram
 	  if(h->GetEntries()==0 && h2->GetEntries()==0){
 	    ofs << "histogram # "<<j<<": "<<fullpath<<" |has zero entries in both files: "<<std::endl;
@@ -91,8 +144,9 @@ void compareAll(TString file1,TString file2){
 	  ksProb = h->KolmogorovTest(h2);
 	  if(ksProb!=1.){
 	    //c1->SaveAs(savename+".pdf");
-	    TPaveText ksPt(0,0, 0.35, 0.04, "NDC"); ksPt.SetBorderSize(0); ksPt.SetFillColor(0);
+	    TPaveText ksPt(0,0.02, 0.35, 0.043, "NDC"); ksPt.SetBorderSize(0); ksPt.SetFillColor(0);
 	    ksPt.AddText(Form("P(KS)=%g, ered %g eblue %g",ksProb, h->GetEntries(), h2->GetEntries()));
+	    ksPt.SetTextSize(0.1);
 	    ksPt.Draw();
 	    c1->Print("diff.pdf");
 	    std::cout<<"histogram # "<<j<<": "<<fullpath<<" |kolmogorov: "<<ksProb<<std::endl;
