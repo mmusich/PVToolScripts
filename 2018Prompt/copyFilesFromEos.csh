1aa903017a90065a8bef20880ccc9fd13d589eac
mmusich <marco.musich@cern.ch>
2018-06-22 15:29:38 +0200

    add folders for 2018Prompt Analysis

---
+++ 2018Prompt/copyFilesFromEos.csh
@@ -0,0 +1,28 @@
+#!/bin/tcsh
+set remote=/store/group/alca_trackeralign/musich/test_out/STARTUP_20182018Monitoring
+#set remote=/store/group/alca_trackeralign/musich/test_out/2017_dataReRecoTest2017ReReco/
+#set remote=/store/group/alca_trackeralign/musich/test_out/2017_dataReReco_bisTest2017ReReco
+
+set myDirs=(PromptGT PCLAlignment)
+
+## create the output folders
+foreach i (`seq $#myDirs`)
+    if (! -d ./$myDirs[$i] ) then
+	mkdir ./$myDirs[$i]
+    else
+	echo "$myDirs[$i] already exists!"			    
+    endif
+end
+
+foreach file (`eos ls $remote`)
+    foreach i (`seq $#myDirs`)
+	if($file =~ *"$myDirs[$i]"*) then 
+	    if(-f ./$myDirs[$i]/$file) then 
+		echo "$file already exists in $myDirs[$i] ---> exiting"
+	    else 
+		echo "copying file $file to $myDirs[$i]" 
+		xrdcp root://eoscms.cern.ch//eos/cms/$remote/$file ./$myDirs[$i]
+	    endif
+	endif
+    end
+end
---
+++ 2018PromptPVResolution/mergeROOT.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+from os import listdir
+from os.path import isfile, join
+from collections import defaultdict
+
+onlyfiles = [f for f in listdir("./") if isfile(join("./", f))]
+
+#iovs= [294034,296641,297179,297281,298653,299277,299443,300389,301046,302131,303790,304911,305898]
+iovs=[314163, 316006, 317393]
+#print onlyfiles                                                                                           
+
+dictionary=defaultdict(list)
+for name in onlyfiles:
+    if not name.endswith('.root'):
+        continue
+    file_info=name.split("_")
+    theRun=file_info[2].split(".")[0]
+    theAlignment=file_info[1]
+    theIOVcount=0
+    theIOV=0
+    for iov in iovs:
+        theIOVcount+=1
+        if(int(theRun)>int(iov)):
+            theIOV=theIOVcount
+
+    dictionary[(theIOV,theAlignment)].append(name)
+    #dictionary[name]={'run':theRun,'alignment':theAlignment,'iov:':theIOV}
+
+
+for key, value in dictionary.iteritems():
+    #print key,value 
+
+    command = 'hadd pvresolution_'+str(key[1])+'_IOV'+str(key[0])+'.root'
+    for f in value:
+        command+=(' '+f)
+
+    print command
+    os.system(command)
